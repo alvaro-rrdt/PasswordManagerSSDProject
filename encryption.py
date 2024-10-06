333fafe5b98afd0f8cd7198aca887e3d86c1f119
alvaro-rrdt <lucarrdtmail@gmail.com>
2024-10-06 12:03:18 +0200

    Initial commit

---
+++ encryption.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+import os
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open("key.key", "rb").read()
+
+def encrypt_message(message):
+    key = load_key()
+    encoded_message = message.encode()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(encoded_message)
+    return encrypted_message
+
+def decrypt_message(encrypted_message):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message).decode()
+    return decrypted_message
+
