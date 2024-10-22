07e543f58b1627c3836c3db35c55d07780831658
alvaro-rrdt <lucarrdtmail@gmail.com>
2024-10-22 19:47:03 +0200

    encryption.py uses a passphrase

--- encryption.py
+++ encryption.py
@@ -1,12 +1,22 @@
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+import base64
 
-def generate_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
 
-def load_key():
-    return open("key.key", "rb").read()
+def derive_key(passphrase, salt=None):
+    if salt is None:
+        salt = b'fixed_salt'
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(passphrase.encode()))
+    return key
+
 
 def encrypt_message(message):
     key = load_key()
